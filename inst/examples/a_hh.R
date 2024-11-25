236a4dcbf97f1940be33a8e2cb219e7bf48784ba
Yonghyun Kwon <yhkwon@iastate.edu>
2024-11-25 14:31:48 +0900

    Update a_hh.R

--- inst/examples/a_hh.R
+++ inst/examples/a_hh.R
@@ -1,7 +1,9 @@
 # Load the survey package
 library(survey)
 
-a_hh = read.csv("C:/Users/ghkfk/Box/Data/KHP2.2_sas/a_hh.csv")
+setwd("C:/Users/User/Box/Data/KHP2.2_sas")
+
+a_hh = read.csv("a_hh.csv")
 
 a_hh$REGION1 <- as.factor(a_hh$REGION1)
 a_hh$REGION2 <- as.factor(a_hh$REGION2)
@@ -32,15 +34,85 @@ survey_freq
 # Compute the weighted totals and their standard errors for HEXP1
 weighted_totals <- svytotal(~as.factor(HEXP1), design = survey_design, vartype = c("se"))
 
-a_ind = read.csv("C:/Users/ghkfk/Box/Data/KHP2.2_sas/a_ind.csv")
+a_ind = read.csv("a_ind.csv")
+
+library(dplyr)
+a_ind <- left_join(a_ind, a_hh %>% select(HHID, REGION2), by = join_by(HHID))
+
+a_ind <- subset(a_ind, I_INC1 != 0) # To be removed
+a_ind <- a_ind[!is.na(a_ind$REGION1), ] # To be removed
+
+tmptable = tapply(a_ind$I_WGC, a_ind %>% select(REGION1), sum) / tapply(a_ind$I_WGC, a_ind %>% select(REGION1), length)
+# table(a_ind %>% select(REGION1, REGION2), useNA = "always")
 
 # Define the survey design object
 survey_design <- svydesign(
   id = ~1,  # Assuming no clustering
-  strata = ~REGION1,  # Strata variables
+  # strata = ~REGION1,
+  # strata = ~REGION1 + REGION2,  # Strata variables
   weights = ~I_WGC,  # Weight variable
-  data = subset(a_ind, I_INC1 != 0)  # Filtered dataset
+  data = a_ind
+  # data = subset(a_ind, I_INC1 != 0)  # Filtered dataset
 )
+
+(weighted_mean <- sum(a_ind$I_INC1 * a_ind$I_WGC) / sum(a_ind$I_WGC))
+
+sqrt(var(a_ind$I_INC1) / nrow(a_ind) * sum(a_ind$I_WGC)^2)
+
+# Step 2: Add a column for strata
+a_ind$strata <- interaction(a_ind$REGION1)
+
+# Step 3: Split data by strata
+library(dplyr)
+
+# Step 3: Split data by strata and calculate variance
+variance <- a_ind %>%
+  group_by(strata) %>%
+  reframe(
+    n_h = n(),
+    N_h = sum(I_WGC),
+    stratum_var = (N_h / n_h)^2 * n_h * var(I_INC1)
+    # w_delta = I_WGC * (I_INC1 - weighted_mean),
+    # mean_w_delta = mean(I_WGC * (I_INC1 - weighted_mean)),
+    # stratum_var = sum((w_delta - mean_w_delta)^2) / (n_h - 1)
+  ) %>%
+  summarise(total_variance = sum(stratum_var)) %>%
+  pull(total_variance)
+
+sum(a_ind$I_WGC)
+summary(a_ind$I_WGC)
+
+# Step 4: Calculate the standard error
+(standard_error <- sqrt(variance))
+log(standard_error)
+
+svymean(~I_INC1, design = survey_design, vartype = c("se"))
+
+tmp <- svytotal(~I_INC1, design = survey_design, vartype = c("se"))
+log(sqrt(attr(tmp,"var")))
+
+svytotal(~I_INC1, design = survey_design, vartype = c("se"))
+
+
+svytotal(~intercept, design = survey_design, vartype = c("se"))
+svymean(~I_INC1, design = survey_design, vartype = c("se"))[1] * sum(subset(a_ind, I_INC1 != 0) %>% select(I_WGC))
+ 
+svyvar(~I_INC1, survey_design)
+
+calibration <- GECal::GEcalib(~ 0, dweight = I_WGC, data = subset(a_ind, I_INC1 != 0),
+                              const = numeric(0),
+                              entropy = "SL", method = "DS")
+GECal::estimate(I_INC1 ~ 1, calibration = calibration, data = subset(a_ind, I_INC1 != 0))$estimate # HT estimator
+
+calibration <- GECal::GEcalib(~ 1, dweight = I_WGC, data = subset(a_ind, I_INC1 != 0),
+                              const = c(sum(subset(a_ind, I_INC1 != 0) %>% select(I_WGC))),
+                              entropy = "SL", method = "DS")
+GECal::estimate(I_INC1 ~ 1, calibration = calibration, data = subset(a_ind, I_INC1 != 0))$estimate # HT estimator
+
+
+sqrt(var(unlist(subset(a_ind, I_INC1 != 0) %>% select(I_INC1))) * 
+       (1 - nrow(subset(a_ind, I_INC1 != 0)) / sum(subset(a_ind, I_INC1 != 0) %>% select(I_WGC))) / nrow(subset(a_ind, I_INC1 != 0))) *
+  sum(subset(a_ind, I_INC1 != 0) %>% select(I_WGC))
 
 # Compute the mean for I_INC1
 mean_income <- svymean(~I_INC1, design = survey_design, vartype = c("se"))
@@ -50,15 +122,21 @@ print(mean_income)
 
 # table(a_ind$HS1, useNA = "always")
 
+table(a_ind$REGION1, a_ind$REGION2)
+
+sum(is.na(a_ind$I_WGC))
+a_ind$I_WGC[a_ind$REGION1[!is.na(a_ind$REGION1)] == 11]
+
 survey_design <- svydesign(
   id = ~1,  # Assuming no clustering
-  strata = ~REGION1,  # Strata variables
+  strata = ~REGION1 + REGION2,  # Strata variables
   weights = ~I_WGC,  # Weight variable
   data = a_ind[!is.na(a_ind$REGION1) & !is.na(a_ind$HS1), ]
+  # data = a_ind[!is.na(a_ind$REGION1), ]
 )
 
 
 # Compute weighted frequencies for HS1
 weighted_frequencies <- svytotal(~as.factor(HS1), design = survey_design,
                                  na.rm=T)
-
+weighted_frequencies
